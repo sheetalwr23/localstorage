887b29e3f5abc139599e848e361026fcdebfd148
Sheetal <Sheetalwr23@gmail.com>
2023-03-22 15:41:30 +0530

    task 11 is completed

--- dom.js
+++ dom.js
@@ -1,29 +1,30 @@
 //~~~~~~form script~~~~~~~~~//
 //console log form details
-const myForm = document.querySelector("#my-form");
-const name1 = document.querySelector("#name");
-const email = document.querySelector("#email");
-const userList = document.querySelector("#userList");
-const msg = document.querySelector(".msg");
+// const myForm = document.querySelector("#my-form");
+// const name1 = document.querySelector("#name");
+// const email = document.querySelector("#email");
+// const userList = document.querySelector("#userList");
+// const msg = document.querySelector(".msg");
 
-myForm.addEventListener("submit", onSubmit);
-const li3 = document.querySelector("#item3");
-function onSubmit(e) {
-  e.preventDefault();
-  console.log(name1.value);
-  console.log(email.value);
+// myForm.addEventListener("submit", onSubmit);
+// const li3 = document.querySelector("#item3");
+// function onSubmit(e) {
+//   e.preventDefault();
+//   console.log(name1.value);
+//   console.log(email.value);
 
-  li3.innerHTML = "name:" + name1.value + " " + " email:" + email.value;
-  if (name1.value == "" || email.value == "") {
-    msg.innerHTML = "Please fill credentials";
-    setTimeout(() => {
-      msg.innerHTML = "";
-    }, 3000);
-  }
-  localStorage.setItem("name", name1.value);
-  localStorage.setItem("email", email.value);
-  // localStorage.clear();
-}
+//   li3.innerHTML = "name:" + name1.value + " " + " email:" + email.value;
+//   if (name1.value == "" || email.value == "") {
+//     msg.innerHTML = "Please fill credentials";
+//     setTimeout(() => {
+//       msg.innerHTML = "";
+//     }, 3000);
+//   }
+//   localStorage.setItem("name", name1.value);
+//   localStorage.setItem("email", email.value);
+
+localStorage.clear();
+// }
 
 // //local storage
 // localStorage.setItem("name", "sheetal");
@@ -34,3 +35,16 @@ function onSubmit(e) {
 
 // //cookie
 // document.cookie = "name=bob";
+let myObj = {
+  name: "sheetal",
+  age: 30,
+};
+let myObj_serialised = JSON.stringify(myObj);
+console.log(myObj_serialised);
+
+localStorage.setItem("myObj", myObj_serialised);
+
+let myObj_deserialised = JSON.parse(localStorage.getItem("myObj"));
+console.log(myObj_deserialised);
+
+// console.log(localStorage);
