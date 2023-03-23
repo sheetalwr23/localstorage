2964a7bbc74bf45cbddf71c8d4197cbc83a76128
Sheetal <Sheetalwr23@gmail.com>
2023-03-23 12:00:17 +0530

    task 12 is completed

--- dom.js
+++ dom.js
@@ -1,50 +1,38 @@
 //~~~~~~form script~~~~~~~~~//
 //console log form details
-// const myForm = document.querySelector("#my-form");
-// const name1 = document.querySelector("#name");
-// const email = document.querySelector("#email");
-// const userList = document.querySelector("#userList");
-// const msg = document.querySelector(".msg");
+const myForm = document.querySelector("#my-form");
+const name1 = document.querySelector("#name");
+const email = document.querySelector("#email");
+const userList = document.querySelector("#userList");
+const msg = document.querySelector(".msg");
 
-// myForm.addEventListener("submit", onSubmit);
-// const li3 = document.querySelector("#item3");
-// function onSubmit(e) {
-//   e.preventDefault();
-//   console.log(name1.value);
-//   console.log(email.value);
+myForm.addEventListener("submit", onSubmit);
+const li3 = document.querySelector("#item3");
 
-//   li3.innerHTML = "name:" + name1.value + " " + " email:" + email.value;
-//   if (name1.value == "" || email.value == "") {
-//     msg.innerHTML = "Please fill credentials";
-//     setTimeout(() => {
-//       msg.innerHTML = "";
-//     }, 3000);
-//   }
-//   localStorage.setItem("name", name1.value);
-//   localStorage.setItem("email", email.value);
+//%%%%%%%%%%%%%%%%%% how to convert array/object into string
+// const reg_arr=[];
+// let string_array=JSON.stringify(reg_arr);
 
-localStorage.clear();
-// }
+//%%%%%%%%store multiple values in local storage
+localStorage.setItem("registration", "[]");
+function onSubmit(e) {
+  e.preventDefault();
+  // console.log(name1.value);
+  // console.log(email.value);
 
-// //local storage
-// localStorage.setItem("name", "sheetal");
-// localStorage.removeItem("name");
+  li3.innerHTML = "name:" + name1.value + " " + " email:" + email.value;
+  if (name1.value == "" || email.value == "") {
+    msg.innerHTML = "Please fill credentials";
+    setTimeout(() => {
+      msg.innerHTML = "";
+    }, 3000);
+  }
+  var obj1 = { name: name1.value, email: email.value };
 
-// //session storage
-// sessionStorage.setItem("name", "John");
-
-// //cookie
-// document.cookie = "name=bob";
-let myObj = {
-  name: "sheetal",
-  age: 30,
-};
-let myObj_serialised = JSON.stringify(myObj);
-console.log(myObj_serialised);
-
-localStorage.setItem("myObj", myObj_serialised);
-
-let myObj_deserialised = JSON.parse(localStorage.getItem("myObj"));
-console.log(myObj_deserialised);
-
-// console.log(localStorage);
+  var registration_items = localStorage.getItem("registration");
+  var reg_obj = JSON.parse(registration_items);
+  reg_obj.push(obj1);
+  console.log(registration_items, reg_obj);
+  localStorage.setItem("registration", JSON.stringify(reg_obj));
+  // localStorage.clear();
+}
